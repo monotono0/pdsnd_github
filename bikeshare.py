1a45d4158a4dfad3a32075d2ed297e20560a1ce7
Mona Henniges <mo.henniges@gmail.com>
2023-08-10 18:59:06 +0000

    Remove placeholder comments

--- bikeshare.py
+++ bikeshare.py
@@ -16,15 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
-    # get user input for month (all, january, february, ... , june)
-
-
-    # get user input for day of week (all, monday, tuesday, ... sunday)
-
-
+ 
     print('-'*40)
     return city, month, day
 
